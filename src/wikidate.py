0d1a242702e28ac37a6060f4e717bd9c7356d78c
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2017-09-11 16:16:50 +0200

    Add wikidate to string

--- src/wikidate.py
+++ src/wikidate.py
@@ -6,3 +6,32 @@ class WikiDate:
 
     def test_values(self):
         return self.days <= 31 and self.days >= 0 and self.month <= 12 and self.month >= 0 
+
+    def get_date_string(self):
+        days = ""
+        if self.days < 10:
+            days = "0" + str(self.days)
+        else:
+            days = str(self.days)
+
+        months = ""
+        if self.month < 10:
+            months = "0" + str(self.month)
+        else:
+            months = str(self.month)
+        
+        year = "+"
+        if self.year < 0:
+            year = "-"
+
+        if self.year < 1000:
+            year += "0"
+
+        if self.year < 100:
+            year += "0"
+
+        if self.year < 10:
+            year += "0"
+
+        year += self.year 
+        return days + ":" + months + ":" + year
