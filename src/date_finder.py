90b9db31b092090be0ac26366eb9125aadaedf19
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2017-09-11 12:34:35 +0200

    Finish date finder

--- src/date_finder.py
+++ src/date_finder.py
@@ -1,4 +1,5 @@
 import re
+from date_context import DateContext
 
 #simple date finder#
 YEAR_PATTERN = r"\d\d\d\d ?(BCE?|AD)?|\d\d\w\w century"
@@ -17,12 +18,16 @@ MY_2_PATTERN = MONTH_NUM_PATTERN + r" " + YEAR_PATTERN
 MY_3_PATTERN = r"[0-1]?[0-9]/ ?" + YEAR_PATTERN
 
 FULL_PATTERN = r"(" + DMY_1_PATTERN + r")|(" + DMY_2_PATTERN + r")|(" + DMY_3_PATTERN + r")|(" + DMY_4_PATTERN + r")|(" + MY_1_PATTERN + r")|(" + MY_2_PATTERN + r")|(" + MY_3_PATTERN + r")|(" + DM_PATTERN + r")|(" + YEAR_PATTERN + r")"
-print(FULL_PATTERN[160:165])
 
 regex = re.compile(FULL_PATTERN, re.IGNORECASE)
 
 def find_dates(text):
-    return regex.findall(text)
+    dates = []
+    for match in regex.finditer(text):
+        datestring = match.group()
+        ct = DateContext(text, datestring)
+        dates.append(ct)
+    return dates
 
 
 def test():
