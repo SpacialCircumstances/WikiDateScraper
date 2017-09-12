e5efa16aec0d08a0bf76ec66a096d1188e1a45e0
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2017-09-12 14:51:53 +0200

    Add month name parsing

--- src/date_parser.py
+++ src/date_parser.py
@@ -4,6 +4,20 @@ import regexes
 
 year_finder = re.compile(".*?(" + regexes.YEAR_PATTERN + ")")
 num_finder = re.compile("\d+")
+month_name_finder = re.compile(regexes.MONTHS_PATTERN, re.IGNORECASE)
+month_map = {"jan" : 1,
+            "feb" : 2,
+            "mar" : 3,
+            "apr" : 4,
+            "may" : 5,
+            "jun" : 6,
+            "jul" : 7,
+            "aug" : 8,
+            "sep" : 9,
+            "oct" : 10,
+            "nov" : 11,
+            "dec" : 12
+            }
 
 def parse_date(datestring):
     date = wikidate.WikiDate()
@@ -12,7 +26,6 @@ def parse_date(datestring):
         yearstring = moy.group(1)
         is_bc = ("BC" in yearstring) or ("BCE" in yearstring)
         yearn = num_finder.search(yearstring).group()
-        print(yearn)
         if is_bc:
             date.year = -int(yearn)
         else:
@@ -20,4 +33,16 @@ def parse_date(datestring):
         
     else:
         date.year = 0
+
+    #Find months
+    #Easy try: month names
+    mnm = month_name_finder.search(datestring)
+    if mnm:
+        mn = mnm.group()[:3].lower()
+        date.month = month_map[mn]
+
+    else:
+        #Try to find numbers
+        pass
+
     return date
